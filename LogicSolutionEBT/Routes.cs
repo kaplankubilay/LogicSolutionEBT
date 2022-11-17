19ac0728f44ac42686f945c6fef09a87aa917cb1
kaplankubilay <67460042+kaplankubilay@users.noreply.github.com>
2022-11-18 01:59:03 +0300

    Routes struct added.

---
+++ LogicSolutionEBT/Routes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicSolutionEBT
+{
+    public struct Routes
+    {
+        public const int N = 0;
+        public const int E = 1;
+        public const int S = 2;
+        public const int W = 3;
+    }
+}
